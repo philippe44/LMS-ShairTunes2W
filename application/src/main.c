dedb233c9709b8580093b587b5ded1c120fb41cc
Philippe G <philippe44@users.noreply.github.com>
2020-12-11 14:30:51 -0800

    0.203.0

--- application/src/main.c
+++ application/src/main.c
@@ -51,7 +51,7 @@ static int 		sock_printf(int sock,...);
 static char*	sock_gets(int sock, char *str, int n);
 static void 	print_usage(int argc, char **argv);
 
-const char *version = "0.202.0";
+const char *version = "0.203.0";
 
 static unsigned short cport, tport, ipc_port;
 static unsigned short port_base, port_range;
@@ -288,7 +288,7 @@ int main(int argc, char **argv) {
 
 		ht = hairtunes_init(host_addr, encoder, use_sync, drift, false, latencies,
 							aeskey, aesiv, fmtp, cport, tport, NULL,
-							hairtunes_cb, NULL, port_base, port_range);
+							hairtunes_cb, NULL, port_base, port_range, -1);
 
 		sock_printf(ipc_sock, "port: %d\n", ht.aport);
 		sock_printf(ipc_sock, "cport: %d\n", ht.cport);
